3c18e1b95c9599cb49a6fa2eba63fa3c09ce0dee
Lenx <lenvzyl@gmail.com>
2018-12-20 14:22:24 +0200

    GET test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,24 @@
+//To Run this test from command line 
+//npm run test-watch
+
 const expect = require('expect')
 const request = require('supertest')
 
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
+const todos = [{
+    text: 'Test Todo'
+},
+{
+    text: 'Test 2 Todo'
+}]
+
 beforeEach((done) => {
     //Removes all Todos from database
-    Todo.remove({}).then(() => done())
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done())
 })
 describe('POST/todos', () => {
     it('should create new todo', (done) => {
@@ -23,29 +35,43 @@ describe('POST/todos', () => {
                 if (err) {
                     return done(err)
                 }
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1)
                     expect(todos[0].text).toBe(text)
                     done()
                 }).catch((e) => done(e))
             })
-    })
+    }),
 
     it('should not create todo with invalid data', (done) => {
         request(app)
             .post('/todos')
-            .send({})
+            .send({text: ""})
             .expect(400)
             .end((err, res) => {
                 if(err) {
                     return done(err)
                 }
-                
+
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0)
+                    expect(todos.length).toBe(2)
                     done()
                 }).catch((e) => done(e))
             })
 
+    })
+
+})
+
+describe('GET/todos', () => {
+    it('get todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                console.log(res.body)
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done)
     })
 })
