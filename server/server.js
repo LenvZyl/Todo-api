937dbac8d6322e67855db4774fb4ec708f12ade9
Lenx <lenvzyl@gmail.com>
2019-01-03 10:27:24 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-
+var {ObjectID} = require('mongodb')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
@@ -22,6 +22,22 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos})
     }, (e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.send('Todo not Found')
+        }
+        res.send({todo})
+    
+    }).catch((e) => {
         res.status(400).send(e)
     })
 })
